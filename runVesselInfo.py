8530f4db3148ee33cae849150303fc5d8d120975
PCsPC <PCsPC@testing.com>
2024-10-02 17:02:06 +0800

    Fixed code format

--- runVesselInfo.py
+++ runVesselInfo.py
@@ -7,87 +7,87 @@ import datetime
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
-    filename = 'E:\\WebClient\\runVesselInfo.log',
+    filename='E:\\WebClient\\runVesselInfo.log',
     datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main():
-	conn = create_connection()
-	cursor = conn.cursor()
-	timeNow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	#retrive Voyage data
+    conn = create_connection()
+    cursor = conn.cursor()
+    timeNow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # retrive Voyage data
 
-	delete_polestar_vesselInfo(cursor)
-	
-	positionData = getSubscription() # Subscription gives last known position of all vessels
+    delete_polestar_vesselInfo(cursor)
 
-	vesselInfo = {}
-	logging.info('Constructing vessel object')
+    positionData = getSubscription()  # Subscription gives last known position of all vessels
 
-	for item in positionData['objects']:
-		vesselInfo['IMONumber'] = str(item.get('ship').get('imo_number'))
-		vesselInfo['MMSI'] = str(item.get('ship').get('mmsi'))
-		vesselInfo['subscriptionId'] = item.get('id')
-		vesselInfo['companyName'] = str(item.get('account').get('company_name'))
-		vesselInfo['shipName'] = str(item.get('ship').get('ship_name'))
-		vesselInfo['technicalManager'] = str(item.get('ship').get('technical_manager'))
-		vesselInfo['shipType'] = str(item.get('ship').get('ship_type'))
-		vesselInfo['callSign'] = str(item.get('ship').get('call_sign'))
-		if item.get('ship').get('flag') is not None:
-			vesselInfo['flagName'] = str(item.get('ship').get('flag').get('name'))
-		else :
-			vesselInfo['flagName'] = ''
-		vesselInfo['portOfRegistry'] = str(item.get('ship').get('port_of_registry'))
-		vesselInfo['classificationSociety'] = str(item.get('ship').get('classification_society'))
-		vesselInfo['registeredOwner'] = str(item.get('ship').get('registered_owner'))
-		vesselInfo['operator'] = str(item.get('ship').get('operator'))
-		if item.get('ship').get('deadweight') is not None:
-			vesselInfo['deadweight'] = str(item.get('ship').get('deadweight'))
-		else:
-			vesselInfo['deadweight'] = ''
-		if item.get('ship').get('displacement') is not None:
-			vesselInfo['displacement'] = str(item.get('ship').get('displacement'))
-		else :
-			vesselInfo['displacement'] = ''
-		if item.get('ship').get('grossTonnage') is not None:
-			vesselInfo['grossTonnage'] = str(item.get('ship').get('gross_tonnage'))
-		else :
-			vesselInfo['grossTonnage'] = ''
-		if item.get('ship').get('length_overall_loa') is not None:
-			vesselInfo['lengthOverall'] = str(item.get('ship').get('length_overall_loa'))
-		else :
-			vesselInfo['lengthOverall'] = ''
+    vesselInfo = {}
+    logging.info('Constructing vessel object')
 
-			
-		if item.get('ship').get('breadth') is not None:
-			vesselInfo['breadth'] = str(item.get('ship').get('breadth'))
-		else :
-			vesselInfo['breadth'] = ''
-		if item.get('ship').get('depth') is not None:
-			vesselInfo['depth'] = str(item.get('ship').get('depth'))
-		else :
-			vesselInfo['depth'] = ''
-		if item.get('ship').get('draught') is not None:
-			vesselInfo['draught'] = str(item.get('ship').get('draught'))
-		else :
-			vesselInfo['draught'] = ''
-		if item.get('ship').get('shipbuilder') is not None:
- 			vesselInfo['shipBuilder'] = str(item.get('ship').get('shipbuilder'))
-		else :
-			vesselInfo['shipBuilder'] = ''
+    for item in positionData['objects']:
+        vesselInfo['IMONumber'] = str(item.get('ship').get('imo_number'))
+        vesselInfo['MMSI'] = str(item.get('ship').get('mmsi'))
+        vesselInfo['subscriptionId'] = item.get('id')
+        vesselInfo['companyName'] = str(item.get('account').get('company_name'))
+        vesselInfo['shipName'] = str(item.get('ship').get('ship_name'))
+        vesselInfo['technicalManager'] = str(item.get('ship').get('technical_manager'))
+        vesselInfo['shipType'] = str(item.get('ship').get('ship_type'))
+        vesselInfo['callSign'] = str(item.get('ship').get('call_sign'))
+        if item.get('ship').get('flag') is not None:
+            vesselInfo['flagName'] = str(item.get('ship').get('flag').get('name'))
+        else:
+            vesselInfo['flagName'] = ''
+        vesselInfo['portOfRegistry'] = str(item.get('ship').get('port_of_registry'))
+        vesselInfo['classificationSociety'] = str(item.get('ship').get('classification_society'))
+        vesselInfo['registeredOwner'] = str(item.get('ship').get('registered_owner'))
+        vesselInfo['operator'] = str(item.get('ship').get('operator'))
+        if item.get('ship').get('deadweight') is not None:
+            vesselInfo['deadweight'] = str(item.get('ship').get('deadweight'))
+        else:
+            vesselInfo['deadweight'] = ''
+        if item.get('ship').get('displacement') is not None:
+            vesselInfo['displacement'] = str(item.get('ship').get('displacement'))
+        else:
+            vesselInfo['displacement'] = ''
+        if item.get('ship').get('grossTonnage') is not None:
+            vesselInfo['grossTonnage'] = str(item.get('ship').get('gross_tonnage'))
+        else:
+            vesselInfo['grossTonnage'] = ''
+        if item.get('ship').get('length_overall_loa') is not None:
+            vesselInfo['lengthOverall'] = str(item.get('ship').get('length_overall_loa'))
+        else:
+            vesselInfo['lengthOverall'] = ''
 
-		vesselInfo['countryOfBuild'] = str(item.get('ship').get('country_of_build'))
-		if item.get('ship').get('year_of_build') is not None:
-			vesselInfo['yearOfBuild'] = str(item.get('ship').get('year_of_build'))
-		else :
-			vesselInfo['yearOfBuild'] = ''
-		vesselInfo['create_time'] = timeNow
-		insert_polestar_vesselInfo(cursor,vesselInfo)
-		logging.info('Inserting vessel: ' + vesselInfo['IMONumber'] + ' : ' + vesselInfo['shipName'])
-	logging.info('Finished writing vessel Info')
-	
-	conn.commit()
-	
-	#insert into database
+        if item.get('ship').get('breadth') is not None:
+            vesselInfo['breadth'] = str(item.get('ship').get('breadth'))
+        else:
+            vesselInfo['breadth'] = ''
+        if item.get('ship').get('depth') is not None:
+            vesselInfo['depth'] = str(item.get('ship').get('depth'))
+        else:
+            vesselInfo['depth'] = ''
+        if item.get('ship').get('draught') is not None:
+            vesselInfo['draught'] = str(item.get('ship').get('draught'))
+        else:
+            vesselInfo['draught'] = ''
+        if item.get('ship').get('shipbuilder') is not None:
+            vesselInfo['shipBuilder'] = str(item.get('ship').get('shipbuilder'))
+        else:
+            vesselInfo['shipBuilder'] = ''
+
+        vesselInfo['countryOfBuild'] = str(item.get('ship').get('country_of_build'))
+        if item.get('ship').get('year_of_build') is not None:
+            vesselInfo['yearOfBuild'] = str(item.get('ship').get('year_of_build'))
+        else:
+            vesselInfo['yearOfBuild'] = ''
+        vesselInfo['create_time'] = timeNow
+        insert_polestar_vesselInfo(cursor, vesselInfo)
+        logging.info('Inserting vessel: ' + vesselInfo['IMONumber'] + ' : ' + vesselInfo['shipName'])
+    logging.info('Finished writing vessel Info')
+
+    conn.commit()
+
+
+# insert into database
 
 main()
--- runVesselPosition.py
+++ runVesselPosition.py
@@ -7,66 +7,75 @@ import datetime
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
-    filename = 'E:\\WebClient\\runVesselPosition.log',
+    filename='E:\\WebClient\\runVesselPosition.log',
     datefmt='%Y-%m-%d %H:%M:%S')
 
+
 def main():
-        conn = create_connection()
-        cursor = conn.cursor()
-        timeNow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        #retrive Position data
-        positionData = getSubscription() # Subscription gives last known position of all vessels
+    conn = create_connection()
+    cursor = conn.cursor()
+    timeNow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # retrive Position data
+    positionData = getSubscription()  # Subscription gives last known position of all vessels
 
-        vesselPosition = {}
-        logging.info('Constructing vessel position object')
-        for item in positionData['objects']:
-                vesselPosition['imo_number'] = str(item.get('ship').get('imo_number'))
-                vesselPosition['create_time'] = timeNow
-                if item.get('last_position') is not None:
-                        vesselPosition['latitude'] = str(item.get('last_position').get('latitude'))
-                        vesselPosition['latitude']=vesselPosition['latitude'][:14]
-                        vesselPosition['longitude'] = str(item.get('last_position').get('longitude'))
-                        vesselPosition['longitude']=vesselPosition['longitude'][:14]
-                        vesselPosition['speed'] = str(item.get('last_position').get('sog_reported'))
-                        vesselPosition['speed']=vesselPosition['speed'][:14]
-                        vesselPosition['heading'] = str(item.get('last_position').get('cog_reported'))
-                        vesselPosition['heading']=vesselPosition['heading'][:14]
-                        vesselPosition['timestamp_gnss'] = str(item.get('last_position').get('timestamp_gnss'))
-                        vesselPosition['alert_status'] = str(item.get('last_position').get('position_info').get('alert_status'))
-                        #Looks like cyclone reported actual observation and forcast, first item is always actual observation
-                        if len(item.get('last_position').get('position_info').get('cyclones')) > 0:
-                                vesselPosition['cyclone_name'] = item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('name')
-                                vesselPosition['cyclone_type'] = item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('storm_type')
-                                vesselPosition['cyclone_distance'] = item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('distance')
-                                vesselPosition['cyclone_latitude'] = item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('latitude')
-                                vesselPosition['cyclone_longitude'] = item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('longitude')
-                                vesselPosition['cyclone_speed'] = item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('speed')
-                        else :
-                                vesselPosition['cyclone_name'] = ''
-                                vesselPosition['cyclone_type'] = ''
-                                vesselPosition['cyclone_distance'] = ''
-                                vesselPosition['cyclone_latitude'] = ''
-                                vesselPosition['cyclone_longitude'] = ''
-                                vesselPosition['cyclone_speed'] = ''
-                else :
-                        vesselPosition['latitude'] = ''
-                        vesselPosition['longitude'] = ''
-                        vesselPosition['speed'] = ''
-                        vesselPosition['heading'] = ''
-                        vesselPosition['timestamp_gnss'] = ''
-                        vesselPosition['alert_status'] = ''
-                        vesselPosition['cyclone_name'] = ''
-                        vesselPosition['cyclone_type'] = ''
-                        vesselPosition['cyclone_distance'] = ''
-                        vesselPosition['cyclone_latitude'] = ''
-                        vesselPosition['cyclone_longitude'] = ''
-                        vesselPosition['cyclone_speed'] = ''
-                logging.debug('Inserting vessel position for : ' + vesselPosition['imo_number'] + ' latitude : ' +
-                              vesselPosition['latitude'] + ' longitude :' + vesselPosition['longitude'] + ' cyclone_name :' + vesselPosition['longitude'])
-                insert_polestar_position(cursor,vesselPosition)
+    vesselPosition = {}
+    logging.info('Constructing vessel position object')
+    for item in positionData['objects']:
+        vesselPosition['imo_number'] = str(item.get('ship').get('imo_number'))
+        vesselPosition['create_time'] = timeNow
+        if item.get('last_position') is not None:
+            vesselPosition['latitude'] = str(item.get('last_position').get('latitude'))
+            vesselPosition['latitude'] = vesselPosition['latitude'][:14]
+            vesselPosition['longitude'] = str(item.get('last_position').get('longitude'))
+            vesselPosition['longitude'] = vesselPosition['longitude'][:14]
+            vesselPosition['speed'] = str(item.get('last_position').get('sog_reported'))
+            vesselPosition['speed'] = vesselPosition['speed'][:14]
+            vesselPosition['heading'] = str(item.get('last_position').get('cog_reported'))
+            vesselPosition['heading'] = vesselPosition['heading'][:14]
+            vesselPosition['timestamp_gnss'] = str(item.get('last_position').get('timestamp_gnss'))
+            vesselPosition['alert_status'] = str(item.get('last_position').get('position_info').get('alert_status'))
+            # Looks like cyclone reported actual observation and forcast, first item is always actual observation
+            if len(item.get('last_position').get('position_info').get('cyclones')) > 0:
+                vesselPosition['cyclone_name'] = \
+                    item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('name')
+                vesselPosition['cyclone_type'] = \
+                    item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('storm_type')
+                vesselPosition['cyclone_distance'] = \
+                    item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('distance')
+                vesselPosition['cyclone_latitude'] = \
+                    item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('latitude')
+                vesselPosition['cyclone_longitude'] = \
+                    item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('longitude')
+                vesselPosition['cyclone_speed'] = \
+                    item.get('last_position').get('position_info').get('cyclones')[0].get('track')[0].get('speed')
+            else:
+                vesselPosition['cyclone_name'] = ''
+                vesselPosition['cyclone_type'] = ''
+                vesselPosition['cyclone_distance'] = ''
+                vesselPosition['cyclone_latitude'] = ''
+                vesselPosition['cyclone_longitude'] = ''
+                vesselPosition['cyclone_speed'] = ''
+        else:
+            vesselPosition['latitude'] = ''
+            vesselPosition['longitude'] = ''
+            vesselPosition['speed'] = ''
+            vesselPosition['heading'] = ''
+            vesselPosition['timestamp_gnss'] = ''
+            vesselPosition['alert_status'] = ''
+            vesselPosition['cyclone_name'] = ''
+            vesselPosition['cyclone_type'] = ''
+            vesselPosition['cyclone_distance'] = ''
+            vesselPosition['cyclone_latitude'] = ''
+            vesselPosition['cyclone_longitude'] = ''
+            vesselPosition['cyclone_speed'] = ''
+        logging.debug('Inserting vessel position for : ' + vesselPosition['imo_number'] + ' latitude : ' +
+                      vesselPosition['latitude'] + ' longitude :' + vesselPosition['longitude'] + ' cyclone_name :' +
+                      vesselPosition['longitude'])
+        insert_polestar_position(cursor, vesselPosition)
 
-        logging.info('Finished inserting position Info')
+    logging.info('Finished inserting position Info')
 
-                      
-        conn.commit()
+    conn.commit()
+
+
 main()
